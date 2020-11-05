f920ff55acdba04667ae88c7d51bb6c4fb79e992
GaspardKerner <66307006+GaspardKerner@users.noreply.github.com>
2020-11-05 11:15:49 +0100

    Update ToRun.sh

--- ToRun.sh
+++ ToRun.sh
@@ -12,10 +12,11 @@ mkdir -p vcf
 # Include VCF files in vcf folder (e.g. one VCF for rare and one VCF for common variants)
 # By default, VCFrare will aggregate rare variants into a gene unit and VCFcommon will consist of single common variants
 # vcf files must be gzipped
-# Use as test example test.rare.vcf.gz and test.common.vcf.gz
+# Use as test example test.rare.vcf.gz and test.common.vcf.gz (you can also use directly the .gds files)
 
 mkdir -p "$feno"
 # Results and phenotype files will be stored here
+
 
 mkdir -p "$feno"/phenotype
 # Include phenotype file 
@@ -28,6 +29,8 @@ mkdir -p "$feno"/phenotype
 #M-P1313 2 -8.85007e-05 -0.0149749 0.00505057
 #M-P1317 2 -0.0108567 -0.0205866 0.00822436
 
+# Use as a test example pheno_test_191cranio.txt
+
 
 mkdir -p scriptR
 #Include Rscripts that can be downloaded here
@@ -36,5 +39,5 @@ mkdir -p "$feno"/di/pvalues
 
 
 # Run the analysis
-Rscript scriptR/my_analysis_di_test.r "$feno" "$VCFcommon" "$VCFrare"
+Rscript scriptR/my_analysis_di_test.r "$feno" "$VCFcommon" "$VCFrare" "$direct"
 
